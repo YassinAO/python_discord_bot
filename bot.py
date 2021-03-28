19d2e4ab3d3818c58f4b80c790bde928e5a4cc16
Y4SSIN <yassin_developer@outlook.com>
2021-03-28 15:34:22 +0200

    Basic events

--- bot.py
+++ bot.py
@@ -2,11 +2,24 @@ import discord
 from discord.ext import commands
 from decouple import config
 
-client = commands.Bot(command_prefix='.')
+intents = discord.Intents(messages=True, guilds=True,
+                          reactions=True, members=True, presences=True)
+
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 
-@client.event
+@bot.event
 async def on_ready():
     print('Bot is ready.')
 
-client.run(config('DISCORD_TOKEN'))
+
+@bot.event
+async def on_member_join(member):
+    print(f'{member} has joined the guild, say hello!')
+
+
+@bot.event
+async def on_member_remove(member):
+    print(f'{member} has left the guild, bye bye!')
+
+bot.run(config('DISCORD_TOKEN'))
